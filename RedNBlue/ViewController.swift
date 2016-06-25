c98b4ca695401c90ab3836a30b02aae3f30f386e
Kyle McSweeney <kylemcsweeney@Kyles-MBP.gateway>
2016-06-25 16:07:28 +1000

    Added files for redNBlue

--- RedNBlue/ViewController.swift
+++ RedNBlue/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redCircle: UIImageView!
+    @IBOutlet weak var blueCircle: UIImageView!
+    
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var ResetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRedAction(sender: AnyObject) {
+        redCircle.hidden = true
+        //blueCircle.hidden = false
+       // hideRedButton.hidden = true
+    }
+    
+    @IBAction func hideBlueAction(sender: AnyObject) {
+        //redCircle.hidden = false
+        blueCircle.hidden = true
+       // hideBlueButton.hidden = true
+    }
+    
+    @IBAction func resetAction(sender: AnyObject) {
+        redCircle.hidden = false
+        blueCircle.hidden = false
+        hideRedButton.hidden = false
+        hideBlueButton.hidden = false
+    }
 
 }
 
